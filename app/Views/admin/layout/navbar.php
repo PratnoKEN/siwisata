ef0c8417137d071dba1d58ca38e21450e09662e6
PratnoKEN <mobilelegendsmoontan@gmail.com>
2023-08-15 23:25:20 +0700

    add button to view client on view admin

--- app/Views/admin/layout/navbar.php
+++ app/Views/admin/layout/navbar.php
@@ -3,6 +3,10 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading"></div>
+                <a class="nav-link" href="<?= base_url(''); ?>">
+                    <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
+                    Halaman Utama
+                </a>
                 <a class="nav-link" href="<?= base_url('dashboard'); ?>">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
