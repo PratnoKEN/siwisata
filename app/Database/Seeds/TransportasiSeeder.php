7ea58d495cc04278dcc630724ef078ea0c8ff3d4
PratnoKEN <mobilelegendsmoontan@gmail.com>
2023-08-20 10:07:44 +0700

    error merge

--- app/Database/Seeds/TransportasiSeeder.php
+++ app/Database/Seeds/TransportasiSeeder.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Database\Seeds;
@@ -39,46 +38,4 @@ class TransportasiSeeder extends Seeder
         // Insert data
         $this->db->table('transportasi')->insertBatch($data);
     }
-=======
-<?php
-
-namespace App\Database\Seeds;
-
-use CodeIgniter\Database\Seeder;
-
-class TransportasiSeeder extends Seeder
-{
-    public function run()
-    {
-        $data = [
-            [
-                'nama_bis' => 'Bis Eksekutif',
-                'harga' => 100000,
-                'tujuan_awal' => 'Surabaya',
-                'tujuan_akhir' => 'Malang',
-                'gambar' => 'bis_eksekutif.jpg',
-                'fasilitas' => 'AC, Wi-Fi, Tempat Duduk Nyaman',
-            ],
-            [
-                'nama_bis' => 'Bis Reguler',
-                'harga' => 75000,
-                'tujuan_awal' => 'Jakarta',
-                'tujuan_akhir' => 'Bandung',
-                'gambar' => 'bis_reguler.jpg',
-                'fasilitas' => 'AC, Tempat Duduk Standard',
-            ],
-            [
-                'nama_bis' => 'Bis Pariwisata',
-                'harga' => 120000,
-                'tujuan_awal' => 'Bali',
-                'tujuan_akhir' => 'Yogyakarta',
-                'gambar' => 'bis_pariwisata.jpg',
-                'fasilitas' => 'AC, Wi-Fi, Tempat Duduk Nyaman, Toilet',
-            ],
-        ];
-
-        // Insert data
-        $this->db->table('transportasi')->insertBatch($data);
-    }
->>>>>>> 59b13ae2290c5f2f0f8d60c4f2e43f8e10c3dec5
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 namespace App\Models;
 
@@ -14,21 +13,4 @@ class UserModel extends Model
     {
         return $this->findAll();
     }
-=======
-<?php
-namespace App\Models;
-
-use CodeIgniter\Model;
-
-class UserModel extends Model
-{
-    protected $table = 'users';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['username', 'password', 'email', 'level'];
-
-    public function getAllUser()
-    {
-        return $this->findAll();
-    }
->>>>>>> 59b13ae2290c5f2f0f8d60c4f2e43f8e10c3dec5
 }
